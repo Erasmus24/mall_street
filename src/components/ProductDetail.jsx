f5f6b3f8d1cc074db800cee6a4d58b0f9385ac14
Erasmus Hadebe <erasmus.hadebe@detegoglobal.com>
2025-02-25 10:31:03 +0200

    Placed the Add to Cart button onto the Product Detail page

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../features/cartSlice";
 import styled from "styled-components";
 
 const ProductDetailContainer = styled.div`
@@ -36,9 +38,37 @@ const ProductDescription = styled.p`
   max-width: 600px;
 `;
 
+const AddToCartButton = styled.button`
+  background-color: #28a745;
+  color: white;
+  padding: 0.8rem 1.5rem;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-top: 1rem;
+
+  &:hover {
+    background-color: #218838;
+  }
+
+  &.bounce {
+    animation: bounce 0.6s ease-out;
+  }
+
+  @keyframes bounce {
+    0% { transform: scale(1); }
+    30% { transform: scale(1.1); }
+    50% { transform: scale(1); }
+    70% { transform: scale(1.05); }
+    100% { transform: scale(1); }
+  }
+`;
+
 const ProductDetail = () => {
   const { id } = useParams();
+  const dispatch = useDispatch();
   const [product, setProduct] = useState(null);
+  const [bouncing, setBouncing] = useState(false);
 
   useEffect(() => {
     const fetchProductDetails = async () => {
@@ -54,6 +84,14 @@ const ProductDetail = () => {
     fetchProductDetails();
   }, [id]);
 
+  const handleAddToCart = () => {
+    if (product) {
+      dispatch(addToCart(product));
+      setBouncing(true);
+      setTimeout(() => setBouncing(false), 600);
+    }
+  };
+
   if (!product) {
     return <p>Loading product details...</p>;
   }
@@ -64,6 +102,9 @@ const ProductDetail = () => {
       <ProductTitle>{product.title}</ProductTitle>
       <ProductPrice>R{product.price}</ProductPrice>
       <ProductDescription>{product.description}</ProductDescription>
+      <AddToCartButton className={bouncing ? "bounce" : ""} onClick={handleAddToCart}>
+        {bouncing ? "Added!" : "Add to Cart"}
+      </AddToCartButton>
     </ProductDetailContainer>
   );
 };
